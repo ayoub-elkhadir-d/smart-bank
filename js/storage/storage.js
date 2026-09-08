73d79871b698abb4c0fb678460ff661c8bf82472
SmartBank Developer <developer@smartbank.local>
2026-09-08 11:30:00 +0100

    Add browser storage utilities

---
+++ js/storage/storage.js
@@ -0,0 +1,28 @@
+
+function save(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+}
+
+
+
+function get(key) {
+    const data = localStorage.getItem(key);
+
+    if (data === null) {
+        return null;
+    }
+
+    return JSON.parse(data);
+}
+
+
+function remove(key) {
+    localStorage.removeItem(key);
+}
+
+
+function clear() {
+    localStorage.clear();
+}
+
+export { save, get, remove, clear };
